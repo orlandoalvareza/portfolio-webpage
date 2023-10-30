d65757962c6f7d45f310fd9760f702a4a5fa8565
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-10-30 16:41:52 -0400

    added basic structure to projects component

---
+++ src/Content/projects.ts
@@ -0,0 +1,19 @@
+import type { PROJECT } from "../Types/types-interfaces";
+
+export const projects: PROJECT[] = [
+  {
+    image: 'https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvamVjdHN8ZW58MHx8MHx8fDA%3D',
+    title: 'project-1',
+    skills: ['lorem', 'ipsum'],
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1605772575717-4548a90a5349?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHByb2plY3RzfGVufDB8fDB8fHww',
+    title: 'project-2',
+    skills: ['lorem2', 'ipsum2'],
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1579389083175-247ef703006f?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2plY3RzfGVufDB8fDB8fHww',
+    title: 'project-3',
+    skills: ['lorem3', 'ipsum3'],
+  }
+]
---
+++ src/Types/types-interfaces.ts
@@ -0,0 +1,5 @@
+export interface PROJECT {
+  image: string;
+  title: string;
+  skills: string[]
+}
